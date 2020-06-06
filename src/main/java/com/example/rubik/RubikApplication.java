5cabd75bd566178846cf91165c0b8567760e262a
lxd <lxd301491@gmail.com>
2020-06-07 00:59:24 +0800

    打包模式改为war包 启用引导类继承SpringBootServletInitializer slf4j打印日志指定位置

--- src/main/java/com/example/rubik/RubikApplication.java
+++ src/main/java/com/example/rubik/RubikApplication.java
@@ -2,12 +2,19 @@ package com.example.rubik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RubikApplication {
+public class RubikApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RubikApplication.class, args);
     }
 
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
--- src/main/java/com/example/rubik/controller/AuthController.java
+++ src/main/java/com/example/rubik/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.rubik.controller;
 
 import com.example.rubik.service.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired
     private AuthService authService;
 
     @RequestMapping("/login")
     public boolean GetPassword(String account, String password) {
+        logger.info("GetPassword 开始");
         return authService.login(account, password);
     }
 }
